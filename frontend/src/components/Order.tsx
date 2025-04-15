08b440779fd5467153a8bcfe1e377bfe6835f8b8
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2025-04-15 16:56:12 -0400

    Implement Order component

---
+++ frontend/src/components/Order.tsx
@@ -0,0 +1,45 @@
+import { OrderItems, Pizza } from "../utils/types";
+import { api } from "../utils/api";
+import { toast } from 'react-toastify';
+export const Order = (_props: {
+    order: OrderItems[],
+    pizzas: Pizza[]
+}) => {
+    const { order, pizzas } = _props;
+
+    const pizzaMap = new Map(pizzas.map(pizza => [pizza.id, pizza]));
+
+    const submitOrder = async () => {
+        try {
+            const { data } = await api.post('/api/orders', { items: order });
+            toast.success('Order placed successfully! Your order id is: ' + data.id);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    return (
+        <div className="w-full lg:w-2/6 h-full flex flex-col justify-between items-center">
+            <h1 className="text-center text-2xl font-bold">Order Summary</h1>
+            <div className="w-full h-full p-6 flex flex-col gap-12">
+                <div>
+
+                    {order.map((item) => {
+                        const pizza = pizzaMap.get(item.pizza_id);
+                        return (
+                            <div key={item.pizza_id} className="flex justify-between">
+                                <p>{pizza!.name}<span className="text-gray-600"> x {item.qty}</span></p>
+                                <p>${item.qty * item.item_price}</p>
+                            </div>
+                        );
+                    })}
+                    <div className="flex justify-between">
+                        <p className="font-bold">Total:</p>
+                        <p className="font-bold">${order.reduce((total, item) => total + item.item_price * item.qty, 0)}</p>
+                    </div>
+                </div>
+                <button onClick={submitOrder} className="py-2 px-4 bg-green-500 hover:bg-green-600 text-white rounded cursor-pointer">Confirm Order</button>
+            </div>
+        </div>
+    );
+}
