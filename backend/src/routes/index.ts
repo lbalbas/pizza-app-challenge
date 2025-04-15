fa1d26e8615bc15f3458e30b743e002154b626fe
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2025-04-15 13:59:13 -0400

    Implement orderRouter

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import pizzaRouter from './pizzaRouter';
+import orderRouter from './orderRouter';
 
 const router = Router();
 
 
 router.use('/api/pizzas', pizzaRouter);
+router.use('/api/orders', orderRouter);
 
 export default router;
