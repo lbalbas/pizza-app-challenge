7264c13c0a4808ff4b5d43f0a3f53089b7537eeb
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2025-04-15 15:18:19 -0400

    Add types to frontend

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,18 @@
+export interface Pizza {
+    id: string,
+    name: string,
+    price: number,
+    ingredients: string[]
+}
+
+export interface Order {
+    id: string
+    items: OrderItems[],
+    total: number,
+}
+
+export interface OrderItems {
+    pizza_id: string,
+    qty: number,
+    item_price: number
+}
