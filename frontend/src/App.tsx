abf38948a52b627704b9d418604456c4509508cb
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2025-04-15 16:09:55 -0400

    Implement PizzaContainer
    Fetches Pizzas from API, displays them and allows the user to add them to their order.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,29 @@
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import { PizzaContainer } from './components/PizzaContainer'
+import { OrderItems, Pizza } from './utils/types';
 
 function App() {
+  const [order, setOrder] = useState<OrderItems[]>([]);
+
+  const addPizzaToOrder = (pizza: Pizza) => {
+    if (order.find((item) => item.pizza_id === pizza.id)) {
+      setOrder(order.map((item) => {
+        if (item.pizza_id === pizza.id) {
+          return { ...item, qty: item.qty + 1 };
+        }
+        return item;
+      }));
+    } else {
+      setOrder([...order, { pizza_id: pizza.id, qty: 1, item_price: pizza.price }]);
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <h1>Mi Pizzería</h1>
+      <h1 className="text-center text-3xl font-bold mt-20">PizzaApp</h1>
+      <div className="flex flex-col lg:flex-row">
+        <PizzaContainer addPizzaToOrder={addPizzaToOrder} />
       </div>
     </>
   )
---
+++ frontend/src/components/PizzaContainer.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { Pizza } from "../utils/types.ts";
+import { api } from '../utils/api.ts'
+
+export const PizzaContainer = (_props: { addPizzaToOrder: (pizza: Pizza) => void }) => {
+    const [pizzas, setPizzas] = useState<Pizza[]>([]);
+
+    const { addPizzaToOrder } = _props;
+
+    useEffect(() => {
+        const fetchPizzas = async () => {
+            try {
+                const { data } = await api.get('/api/pizzas');
+                setPizzas(data);
+            } catch (err) {
+                console.error('Failed to load pizzas:', err);
+            }
+        };
+        fetchPizzas();
+    }, []);
+
+    if (pizzas.length === 0) {
+        return (
+            <main>
+                <h1>Loading...</h1>
+            </main>
+        )
+    }
+
+    return (
+        <main className="w-full">
+            <h1 className="text-center text-2xl font-bold">Menu</h1>
+            <div className="flex flex-wrap justify-between">
+                {pizzas.map((pizza) => (
+                    <article className="border-gray-200 border p-4 rounded-xl shadow-md flex flex-col w-64 justify-between" key={pizza.id}>
+                        <div className="flex flex-col gap-1">
+                            <div className="flex items-baseline gap-2">
+                                <h2 className="text-lg font-bold">{pizza.name}</h2>
+                                <span className="text-gray-600">${pizza.price}</span>
+                            </div>
+                            <div className="pb-2">
+                                <h3>Ingredients</h3>
+                                <p className="text-gray-600">{pizza.ingredients.join(', ')}</p>
+                            </div>
+                        </div>
+
+                        <button className="py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white rounded cursor-pointer" onClick={() => addPizzaToOrder(pizza)}>Add to order</button>
+                    </article>
+                ))}
+            </div>
+        </main>
+    )
+}
