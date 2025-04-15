5e68d98b394fa313dae652dc71e92d9c1fe1a242
Luis Balbás <30774937+lbalbas@users.noreply.github.com>
2025-04-15 16:55:57 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
+import { ToastContainer } from 'react-toastify'
 import { PizzaContainer } from './components/PizzaContainer'
 import { OrderItems, Pizza } from './utils/types';
+import { Order } from './components/Order';
+import { api } from './utils/api';
 
 function App() {
   const [order, setOrder] = useState<OrderItems[]>([]);
+  const [pizzas, setPizzas] = useState<Pizza[]>([]); // Lift state up
+
+  // Move your pizza fetching logic here
+  useEffect(() => {
+    const fetchPizzas = async () => {
+      try {
+        const { data } = await api.get('/api/pizzas');
+        setPizzas(data);
+      } catch (err) {
+        console.error('Failed to load pizzas:', err);
+      }
+    };
+    fetchPizzas();
+  }, []);
 
   const addPizzaToOrder = (pizza: Pizza) => {
     if (order.find((item) => item.pizza_id === pizza.id)) {
@@ -21,10 +38,12 @@ function App() {
 
   return (
     <>
-      <h1 className="text-center text-3xl font-bold mt-20">PizzaApp</h1>
+      <h1 className="text-center text-3xl font-bold mt-8">PizzaApp</h1>
       <div className="flex flex-col lg:flex-row">
-        <PizzaContainer addPizzaToOrder={addPizzaToOrder} />
+        <Order pizzas={pizzas} order={order} />
+        <PizzaContainer pizzas={pizzas} addPizzaToOrder={addPizzaToOrder} />
       </div>
+      <ToastContainer />
     </>
   )
 }
